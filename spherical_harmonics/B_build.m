914a8cccd9dd2bda7aedf59b8ebc90dfaf39e60d
Sarah Burnett <sarahburnett@pc80.home>
2019-10-09 00:20:38 +0200

    builds after obtaining the gauss coeffs

---
+++ spherical_harmonics/B_build.m
@@ -0,0 +1,26 @@
+function Bsum = B_build(r,theta,phi)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Code to evaluate the magnetic field given gauss coefficients from gcoeff
+% Author: Sarah Burnett Oct 2019
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('gauss.mat')
+lmax = 4;
+
+Bsum = 0;
+for el = 1:lmax
+    B = 0; 
+    Plm = legendre(el,transpose(cos(theta)),'sch');
+    idx = el^2;
+    for m = 0:el
+       if m == 0
+           B = B + g(:,idx)*Plm(m+1); 
+       else
+           B = B + Plm(m+1)*(g(:,idx+1)*cos(m*phi)+g(:,idx+2)*sin(-m*phi));
+           idx = idx + 2; 
+       end
+    end
+    Bsum = Bsum + el*(el+1)*r^(-(el+2))*B; 
+end
+
+end
