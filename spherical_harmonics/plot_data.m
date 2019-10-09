fdc817b16e7a24b10ac1eeff030f00c612b41bcd
Sarah Burnett <sarahburnett@eduroam-108229.grenet.fr>
2019-10-09 10:28:20 +0200

    Added some comments

--- spherical_harmonics/plot_data.m
+++ spherical_harmonics/plot_data.m
@@ -21,13 +21,20 @@ end
 
 phi_deg = phi.*180/pi-180;
 theta_deg = theta.*180/pi-90;
+figure(1),
+m_proj('Hammer-Aitoff','lat',[-90 90], 'lon',[-180 180])
+m_contourf(phi_deg,theta_deg,BR(:,:,1)','EdgeColor','None')
+str = sprintf('t = %2.3fs',time(1)-time(1));
+title(str)
+colorbar
+caxis([-0.02 0.06])
 
 %% Make a movie
 
-vidfile = VideoWriter('Ro-49e-2.avi');
+vidfile = VideoWriter('Ro-49e-2.mp4'); %name the movie
 open(vidfile);
 
-for idx = 1:length(time)
+for idx = 1:20:length(time) %fewer frames for a smaller file
     figure(1),
     m_proj('Hammer-Aitoff','lat',[-90 90], 'lon',[-180 180])
     m_contourf(phi_deg,theta_deg,BR(:,:,idx)','EdgeColor','None')
