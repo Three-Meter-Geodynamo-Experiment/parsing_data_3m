2767121bb7412f075ecb8773ddb3553f1777cc9e
ejaross <111381517+ejaross@users.noreply.github.com>
2022-08-16 16:46:12 -0400

    Update way_to_folders.m

--- getting_data_chunks/way_to_folders.m
+++ getting_data_chunks/way_to_folders.m
@@ -1,6 +1,6 @@
 function [way] = way_to_folders()
 % Returns the location of the folder with data folders
 % modify this variable 'way' to fit your architecture
-way = '/data/3m/';
+way = '/Users/elaine/Documents/MATLAB/3meter/';
 end
 
